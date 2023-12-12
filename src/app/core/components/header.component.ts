509ccb42961ef94a3c867024206b979ff0771c6e
Pavel <verbipo@gmail.com>
2023-12-12 10:34:18 +0100

    feat: add footer and header

---
+++ src/app/core/components/header.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<header class="header-content-wrapper">
+    <div class="logo-container">
+      <a routerLink="#" aria-label="Logo">
+        <img class="logo" src="assets/logo.png" alt="site logo" />
+      </a>
+    </div>
+  </header> `,
+  styles: [
+    `
+      ::ng-deep app-header {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-color: #e5e5e5;
+      }
+
+      .header-content-wrapper {
+        width: min(98vw, 1200px);
+        margin-inline: auto;
+        height: 80px;
+        padding-inline: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+
+      .logo-container {
+        width: 50px;
+      }
+
+      .logo {
+        width: 50px;
+      }
+
+      .main-bar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+      }
+    `,
+  ],
+})
+export class HeaderComponent {}
