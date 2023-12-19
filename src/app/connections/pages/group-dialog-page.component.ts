3df48617f06fd8ff9d27d8c6d0bcd7b03e42e3a7
Pavel <verbipo@gmail.com>
2023-12-19 17:27:17 +0100

    feat: add link to go back

--- src/app/connections/pages/group-dialog-page.component.ts
+++ src/app/connections/pages/group-dialog-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-group-dialog',
   template: `
+    <div class="group-dialog-header">
+      <a href="#" (click)="goBack($event)"> <mat-icon>arrow_back</mat-icon> </a>
+    </div>
     <div>
       group dialog page worck
       <p>{{ groupId }}</p>
@@ -21,13 +24,24 @@ import { Component, OnInit } from '@angular/core';
         height: 100%;
         padding-top: 20px;
       }
+      .group-dialog-header {
+        a {
+          color: black;
+          text-decoration: none;
+        }
+      }
     `,
   ],
 })
 export class GroupDialogPageComponent implements OnInit {
   groupId: string = 'someid';
 
-  constructor() {}
+  constructor(private location: Location) {}
 
   ngOnInit() {}
+
+  goBack(event: Event): void {
+    event.preventDefault();
+    this.location.back();
+  }
 }
